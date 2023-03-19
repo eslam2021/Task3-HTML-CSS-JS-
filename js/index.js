eee09509c5be3365134ac80f20374e12129199a3
eslam2021 <eslam.mahmoud344@gmail.com>
2023-03-19 21:08:44 +0200

    firist commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+ let img = document.querySelector('#mainImage');
+ let container = document.querySelector('#Main');
+ function  Phones(Phone){
+    console.log(Phone);
+    img.src =Phone;
+ }
+ function  colors(color){
+   console.log(color);
+   container.style.background = color;
+ }
+ let sunm=5;
